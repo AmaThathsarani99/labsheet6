9ea2e2b4d654ad13d6d4ac511bc5881cfe7f1a4d
EC/2020/062 - THATHSARANI W.K.A <thathsa-ec20062@kln.ac.lk>
2024-12-03 17:37:06 +0530

    add files

--- src/main/java/com/example/MongodbProject/model/Student.java
+++ src/main/java/com/example/MongodbProject/model/Student.java
@@ -1,12 +1,23 @@
 package com.example.MongodbProject.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Student {
     @Id
     private String id;
